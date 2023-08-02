351364c0e589c1ee8d347088b5d9bbddf55cc6f6
geneticsjesse <jw64450@gmail.com>
2023-08-02 15:00:00 -0400

    Add shebang at start of file; make script executable; remove commented out commands and intermediate files; clean up script to make reproducible

--- examples/parkinsons-disease/connecting_metadata.sh
+++ examples/parkinsons-disease/connecting_metadata.sh
@@ -1,18 +1,16 @@
-# Fix line break characters to facilitate grep etc
-#sed 's/\^M/\n/g' biosample_result\ \(2\).txt > biosample_result_linebreak.txt
-mv biosample_result \ \(2\).txt > biosample_raw.txt
-# grep related rows and create a new text file
-grep -E 'host sex|host age|dataset|health state|SRA:' biosample_raw.txt > output_raw.txt
-#grep -E 'host sex|host age|dataset|health state|SRA:' biosample_result_linebreak.txt > output.txt
+#!/bin/bash
 
-# Remove sample identifiers except for SRA: SRS[0-9]+
-sed 's/.*\(SRA: SRS[0-9]\+\)/\1/' output_raw.txt > output_cleaner.txt
+# grep related rows and create a new text file
+grep -E 'host sex|host age|dataset|health state|SRA:' biosample_result.txt > output_grep.txt
 
 # Manipulate file so every 6 rows go into columns - paste - - - - - -: The paste command combines lines from input files horizontally, separating them by a delimiter (default is a tab). In this case, we use six hyphens ("-") to represent six input fields.
-paste - - - - - - < output_cleaner.txt > output_cols.txt
+paste - - - - - - < output_grep.txt > output_cols.txt
 
+# Manually create a file with column names
 echo -e "SRA\t host_age\t host_sex\t health_status\t dataset\t sequencing_pool" > header.txt
+
+# Combine our header and output file to create a text file that can be imported into PD_metadata_join.R for further manipulation
 cat header.txt output_cols.txt > output_cols_header.txt
 
-grep "dataset_1" PD_reads_metadata.csv | cut -f1 -d ',' > dataset_1_files.txt
-sed -e 's/^"//' -e 's/"$//' < dataset_2_files.txt > dataset_2_SraAccList.txt
+# Remove intermediate files
+rm header.txt output_cols.txt output_cleaner.txt output_raw.txt 
