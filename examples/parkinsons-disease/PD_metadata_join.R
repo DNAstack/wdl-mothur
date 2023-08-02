af44060162a6eb3e5a191b43149d8c0a3c8892f0
geneticsjesse <jw64450@gmail.com>
2023-08-02 15:01:15 -0400

    Add comments; clean up and make script reproducible

--- examples/parkinsons-disease/PD_metadata_join.R
+++ examples/parkinsons-disease/PD_metadata_join.R
@@ -1,17 +1,26 @@
+# Load in dplyr and read in output file from connecting_metadata.sh
 library(dplyr)
 bio <- read.table('output_cols_header.txt', sep = "\t",  header=TRUE)
+
+# Use gsub to clean up output file from connecting_metadata.sh
 bio$host_age <- gsub("\\D+", "", bio$host_age)
 bio$host_sex<- gsub("    /host sex=", "", bio$host_sex)
 bio$health_status<- gsub ("    /health state=", "", bio$health_status)
 bio$dataset<- gsub ("    /dataset=", "", bio$dataset)
 bio$sequencing_pool <- gsub ("    /sequencing_pool=", "", bio$sequencing_pool)
-bio$SRA<- gsub ("SRA: ", "", bio$SRA)
+bio$SRA<- gsub (".*SRA: ", "", bio$SRA)
 
+# Read in csv containing SRA run info
 sra<- read.csv('SraRunInfo.csv')
+
+# Rename sample to SRA to facilitate left_join
 sra <- sra %>%
   rename (SRA = Sample)
 
+# Join sra and bio data frames and filter out dataset_1 samples that were not used in this example 
 bio_sra<- left_join(bio, sra, by = 'SRA')%>%
-  select (SRA, host_age, host_sex, health_status, dataset, sequencing_pool, Run, spots, bases, spots_with_mates, avgLength, size_MB, Experiment, LibraryName, BioSample, SampleName)
+  select (SRA, host_age, host_sex, health_status, dataset, sequencing_pool, Run, spots, bases, spots_with_mates, avgLength, size_MB, Experiment, LibraryName, BioSample, SampleName)%>%
+  filter (dataset == 'dataset_2')
 
-write.csv(bio_sra, 'PD_reads_metadata.csv', row.names = FALSE)
+# Write joined data frame to csv
+write.csv(bio_sra, 'PD_metadata.csv', row.names = FALSE)
