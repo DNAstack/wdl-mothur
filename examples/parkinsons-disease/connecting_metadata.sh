d0149e51a2cb47984bdf8c840f7750254e714834
geneticsjesse <jw64450@gmail.com>
2023-08-04 12:35:23 -0400

    Change name of raw file to use; remove 'example' from output file name

--- examples/parkinsons-disease/connecting_metadata.sh
+++ examples/parkinsons-disease/connecting_metadata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # grep related rows and create a new text file
-grep -E 'host sex|host age|dataset|health state|SRA:' biosampleDataExample.txt > output_grep.txt
+grep -E 'host sex|host age|dataset|health state|SRA:' biosample_result.txt > output_grep.txt
 
 # Manipulate file so every 6 rows go into columns - paste - - - - - -: The paste command combines lines from input files horizontally, separating them by a delimiter (default is a tab). In this case, we use six hyphens ("-") to represent six input fields.
 paste - - - - - - < output_grep.txt > output_cols.txt
@@ -10,7 +10,7 @@ paste - - - - - - < output_grep.txt > output_cols.txt
 echo -e "SRA\t host_age\t host_sex\t health_status\t dataset\t sequencing_pool" > header.txt
 
 # Combine our header and output file to create a text file that can be imported into PD_metadata_join.R for further manipulation
-cat header.txt output_cols.txt > connecting_metadata_example_output.txt
+cat header.txt output_cols.txt > connecting_metadata_output.txt
 
 # Remove intermediate files
 rm header.txt output_cols.txt output_grep.txt
